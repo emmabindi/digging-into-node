e2fa809f889a2604438663f171d173042156f925
Emma Bindi <emma.bindi@outlook.com>
2020-08-04 09:24:28 +1000

    Exercise 1 complete

---
+++ exercises/ex1.js
@@ -0,0 +1,70 @@
+#!/usr/bin/env node
+
+"use strict";
+
+var util = require("util");
+var path = require("path");
+var fs = require("fs");
+var getStdin = require("get-stdin");
+
+var args = require("minimist")(process.argv.slice(2), {
+  boolean: [ "help", "in" ], 
+  string: [ "file" ]
+});
+// console.log(args);
+
+var BASE_PATH = path.resolve(
+  process.env.BASE_PATH || __dirname
+);
+
+if (args.help) {
+  printHelp();
+} else if (args.file) {
+  fs.readFile(path.join(BASE_PATH,args.file), function onContents(err,contents){
+    if (err) {
+      error(err.toString());
+    }
+    else {
+      processFile(contents.toString());
+    }
+  });
+  // console.log(__dirname);
+} else if (args.in || args._.includes("-")) {
+  getStdin().then(processFile).catch(error);
+} else {
+  error("Incorrect usage.", true)
+}
+
+// ******************
+
+function processFile(contents) {
+  contents = contents.toUpperCase();
+  process.stdout.write(contents);
+}
+
+function error(msg, includeHelp = false) {
+  console.error(msg);
+  if (includeHelp) {
+    console.log("")
+    printHelp();
+  }
+}
+
+function printHelp() {
+  console.log("ex1 usage:");
+  console.log("  ex1.js --file={FILENAME}");
+  console.log("");
+  console.log("--help               print this help");
+  console.log("--file={FILENAME}    process the file");
+  console.log("--in, -              process stdin");
+  console.log();
+}
+
+// console.log(process.argv.slice(2));
+
+// My first Node.js program
+// process.stdin.read()
+
+// process.stdout.write("Hello world\n");
+
+// console.error("Oops");
